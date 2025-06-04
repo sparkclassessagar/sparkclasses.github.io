23ed57268928381bad9fa5d632924258d2dbcf67
sparkclassessagar <clsahu9329@gmail.com>
2025-06-04 19:57:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+<script>
+    function switchLanguage(lang) {
+      document.querySelectorAll('[data-' + lang + ']').forEach(el => {
+        el.textContent = el.getAttribute('data-' + lang);
+      });
+    }
+    // Initialize default to Hindi
+    window.onload = () => switchLanguage('hi');
+  </script>
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
